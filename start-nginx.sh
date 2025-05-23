2caa1670d173e8837c36219b2e60dab2a439550e
Sourabh <sppathak1428@gmail.com>
2025-05-23 13:32:44 +0530

    as

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Replace the backend host in nginx.conf with the environment variable if set
+if [ -n "$BACKEND_URL" ]; then
+    # Remove protocol if present
+    BACKEND_HOST=$(echo $BACKEND_URL | sed -E 's/^https?:\/\///')
+    sed -i "s/set \$backend_host \"backend:5000\";/set \$backend_host \"$BACKEND_HOST\";/" /etc/nginx/conf.d/default.conf
+fi
+
 # Wait for the backend to be ready
 echo "Waiting for backend to be ready..."
 while ! wget --spider -q http://backend:5000/; do
