8fa5d7635de9500b22a82e739980f30485d977f5
Sourabh <sppathak1428@gmail.com>
2025-05-23 14:05:00 +0530

    as

--- start-nginx.sh
+++ start-nginx.sh
@@ -11,14 +11,14 @@ if [ -n "$BACKEND_URL" ]; then
     sed -i "s/set \$backend_host \"backend:5000\";/set \$backend_host \"$BACKEND_HOST\";/" /etc/nginx/conf.d/default.conf
 fi
 
-# Wait for the backend to be ready
+# Wait for backend to be ready
 echo "Waiting for backend to be ready..."
-while ! wget --spider -q http://backend:5000/; do
-  echo "Backend not ready yet... waiting"
-  sleep 2
+while ! wget -q --spider http://backend:5000/; do
+    echo "Backend not ready yet... waiting"
+    sleep 2
 done
 echo "Backend is ready!"
 
 # Start Nginx
 echo "Starting Nginx..."
-nginx -g 'daemon off;' 
+exec nginx -g 'daemon off;' 
