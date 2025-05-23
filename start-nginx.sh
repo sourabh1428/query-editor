14038c740b4bac5cbad8ef8f4d3da78ba1b5570a
Sourabh <sppathak1428@gmail.com>
2025-05-23 13:57:01 +0530

    as

--- start-nginx.sh
+++ start-nginx.sh
@@ -3,8 +3,11 @@ set -e
 
 # Replace the backend host in nginx.conf with the environment variable if set
 if [ -n "$BACKEND_URL" ]; then
-    # Remove protocol if present
+    # Remove protocol if present and ensure port is included
     BACKEND_HOST=$(echo $BACKEND_URL | sed -E 's/^https?:\/\///')
+    if [[ ! $BACKEND_HOST =~ :[0-9]+$ ]]; then
+        BACKEND_HOST="${BACKEND_HOST}:5000"
+    fi
     sed -i "s/set \$backend_host \"backend:5000\";/set \$backend_host \"$BACKEND_HOST\";/" /etc/nginx/conf.d/default.conf
 fi
 
